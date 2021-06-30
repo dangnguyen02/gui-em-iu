507630d673c412e5db989214f88dcaa3319747e3
dangnguyen02 <86719058+dangnguyen02@users.noreply.github.com>
2021-06-30 20:07:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'He luu 16311!',
-    introDesc: `Tớ có điều này muốn nói với các cậu
+    introTitle: 'He luu Hòa Bằng!',
+    introDesc: `Tớ có điều này muốn nói với  cậu
     trả lời tớ thật lòng nhé`,
     btnIntro: 'Ok được thôi',
-    title: 'Các cậu thấy tớ có đẹp trai không 😙',
+    title: ' cậu thấy tớ có đẹp trai không 😙',
     desc: 'Trả lời đi hehe ',
     btnYes: 'Rất đẹp trai <3',
     btnNo: 'Không, Xấu vãi nồi :(',
-    question:'Các cậu có yêu tớ khum <3',
+    question:' hỏi lần nữa là có đẹp trai khum <3',
     btnReply: 'Gửi cho tớ nào <3',
-    reply: 'Yêu Nguyễn Hải Đăng nhiều lắm hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu các bạn nhiều nhiều 😘😘',
-    messDesc: 'I Love you .',
-    btnAccept: 'Moaaaa <3',
+    reply: 'đẹp trai vkl lun hihi <33333333',
+    mess: 'Tớ biết mà 🥰. haha 😘😘',
+    messDesc: 'HIHI .',
+    btnAccept: 'Cảm ơn <3',
     messLink: 'https://www.facebook.com/messages/t/3279580882138120/'
 }
