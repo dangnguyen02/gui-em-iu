afc6fc026905b6a2b48222e8b0a7cfee492af231
dangnguyen02 <86719058+dangnguyen02@users.noreply.github.com>
2021-07-05 20:51:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const CONFIG = {
     introTitle: 'He luu Em iu!',
     introDesc: `Anh có điều này muốn nói với  em
     trả lời anh thật lòng nhé`,
-    btnIntro: 'Ok được thôi',
-    title: ' Em yêu anh nhiều lắm phải không 😙',
-    desc: 'Trả lời thật đi hehe ',
+    btnIntro: 'vâng ạ',
+    title: ' Em yêu anh nhiều lắm hông 😙',
+    desc: 'Trả lời đi hehe ',
     btnYes: 'Yêu ơi là yêuuu <3',
     btnNo: 'Không, Em ko yêu :(',
-    question:' Em yêu anh nhiều khum <3',
+    question:'  yêu anh như thế nào <3',
     btnReply: 'Gửi cho anh nào <3',
     reply: 'yêu Nguyễn Hải Đăng lắm lắm luôn <33333333',
     mess: 'Anh biết mà 🥰. Yêu Em 3000 😘😘',
     messDesc: 'Moaaaaa .',
-    btnAccept: 'Love <3',
-    messLink: 'https://www.facebook.com/messages/t/100018370124661'
+    btnAccept: 'Love Bùi Thị Uyên <3',
+    messLink: 'https://www.youtube.com/watch?v=jdzj6tYAnjY'
 }
